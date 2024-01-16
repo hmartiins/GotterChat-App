1438d9b05ed3244408f605d91d3971fcf9e5375d
hmartiins <hmartins224@gmail.com>
2024-01-16 17:17:57 -0300

    refactor: create home tab bar widget

--- lib/app/modules/home/presentation/pages/home_page.dart
+++ lib/app/modules/home/presentation/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gotter_chat/app/commons/adapters/localizations/translate_app.dart';
 import 'package:gotter_chat/app/commons/styles/tokens.dart';
 import 'package:gotter_chat/app/commons/widgets/complex/screen.dart';
+import 'package:gotter_chat/app/modules/home/presentation/widgets/home_tab_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -40,47 +41,7 @@ class HomePage extends StatelessWidget {
               ],
             ),
             SizedBox(height: Tp.size.ref16),
-            DefaultTabController(
-              initialIndex: 1,
-              length: 2,
-              child: Column(
-                children: [
-                  DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: Tp.colors.grey10,
-                      borderRadius: BorderRadius.circular(100),
-                    ),
-                    child: TabBar(
-                      tabs: [
-                        Tab(
-                          text: intl(context, 'home.tab-messages'),
-                        ),
-                        Tab(
-                          text: intl(context, 'home.tab-group'),
-                        ),
-                      ],
-                      labelColor: Tp.colors.white,
-                      indicator: BoxDecoration(
-                        color: Tp.colors.grey,
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                      indicatorColor: Tp.colors.gray70,
-                      indicatorSize: TabBarIndicatorSize.tab,
-                    ),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: 260,
-                    child: const TabBarView(
-                      children: [
-                        Text('Gotter'),
-                        Text('Gotter'),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
+            const HomeTabBar(),
           ],
         ),
       ),
---
+++ lib/app/modules/home/presentation/widgets/home_tab_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:gotter_chat/app/commons/adapters/localizations/translate_app.dart';
+import 'package:gotter_chat/app/commons/styles/tokens.dart';
+
+class HomeTabBar extends StatefulWidget {
+  const HomeTabBar({super.key});
+
+  @override
+  State<HomeTabBar> createState() => _HomeTabBarState();
+}
+
+class _HomeTabBarState extends State<HomeTabBar> {
+  int _selectedTabbar = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Column(
+        children: [
+          DecoratedBox(
+            decoration: BoxDecoration(
+              color: Tp.colors.grey10,
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: TabBar(
+              onTap: (index) => setState(() {
+                _selectedTabbar = index;
+              }),
+              tabs: [
+                Tab(
+                  text: intl(context, 'home.tab-messages'),
+                ),
+                Tab(
+                  text: intl(context, 'home.tab-group'),
+                ),
+              ],
+              labelColor: Tp.colors.white,
+              indicator: BoxDecoration(
+                color: Tp.colors.grey,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              indicatorColor: Tp.colors.gray70,
+              indicatorSize: TabBarIndicatorSize.tab,
+            ),
+          ),
+          Builder(
+            builder: (_) {
+              if (_selectedTabbar == 0) {
+                return const Text("Tab");
+              } else {
+                return const Text("Tab2");
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
